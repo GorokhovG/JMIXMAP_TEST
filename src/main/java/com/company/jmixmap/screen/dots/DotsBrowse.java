379fcf0116d0f22e9c6818d7cfbfff2e5d84dde5
GorokhovG <89626189+GorokhovG@users.noreply.github.com>
2021-12-19 17:06:41 +0200

    New Fixes

--- src/main/java/com/company/jmixmap/screen/dots/DotsBrowse.java
+++ src/main/java/com/company/jmixmap/screen/dots/DotsBrowse.java
@@ -2,7 +2,9 @@ package com.company.jmixmap.screen.dots;
 
 import io.jmix.mapsui.component.GeoMap;
 import io.jmix.mapsui.component.layer.VectorLayer;
+import io.jmix.mapsui.component.layer.style.GeometryStyle;
 import io.jmix.mapsui.component.layer.style.GeometryStyles;
+import io.jmix.mapsui.component.layer.style.PointStyle;
 import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
@@ -10,7 +12,10 @@ import com.company.jmixmap.entity.Dots;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 
 @UiController("gg_DotsEntity.browse")
 @UiDescriptor("dots-browse.xml")
@@ -34,18 +39,24 @@ public class DotsBrowse extends StandardLookup<Dots> {
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
-       // dotsesTable.setMultiSelect(true);
+    }
 
-        VectorLayer<Dots> dotsLayer = new VectorLayer<>("dotsLayer", dotsesDc);
-        dotsLayer.setEditable(true);
-        map.addLayer(dotsLayer);
-        map.selectLayer(dotsLayer);
+    @Install(to = "map.dotsesLayer", subject = "styleProvider")
+    private GeometryStyle mapOrderLayerStyleProvider(Dots dots) throws MalformedURLException {
+        Random rnd = new Random();
+        int id = 1 + rnd.nextInt(11);
+        String charOne = id > 9? "0" : "";
+
+        String image = "https://gorokhovg.github.io/icons/" + charOne + id + ".svg";
+        URL url = new URL(image);
+
+        return geometryStyles.point().withImageIcon(url).setIconSize(50, 50);
+                //.withDivIcon("<div style=\"margin-left:-20px;margin-top:-35px;\"><img src=\""+ image +"\" width=50 height=50/></div>");
     }
 
     @Subscribe("map.dotsesLayer")
     public void onMapOrderLayerGeoObjectSelected(VectorLayer.GeoObjectSelectedEvent<Dots> event) {
         dotsCollection.add(event.getItem());
-        //dotsesTable.;
         dotsesTable.setSelected(dotsCollection);
 
         //log.info(event.getItem().getAddress());
